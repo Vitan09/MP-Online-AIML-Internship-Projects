eb50e7a680a4e63066ee25722a624c36f292b383
Vitan09 <vitanshukla@gmail.com>
2026-07-28 23:36:41 +0530

    Updated vision.py

---
+++ Capstone project/app/routers/vision.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, UploadFile, File
+import cv2
+import numpy as np
+
+router = APIRouter(prefix="/vision", tags=["Vision"])
+
+
+@router.post("/edge-detection")
+async def edge_detection(file: UploadFile = File(...)):
+
+    image = await file.read()
+
+    image = np.frombuffer(image, np.uint8)
+
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(gray, 100, 200)
+
+    edge_pixels = int(np.count_nonzero(edges))
+
+    return {
+        "message": "Edge detection completed",
+        "edge_pixels": edge_pixels
+    }
